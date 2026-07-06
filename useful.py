dc1e0dc12beb415f8508017d7fcbfe2b15c989fe
Daniel Chan <chumchan2@gmail.com>
2026-07-07 07:24:13 +0800

    daone
    done

--- useful.py
+++
@@ -1,9 +0,0 @@
-import os
-import glob
-
-def getAllFiles(path, ext):
-    os.chdir(path)
-    l = glob.glob('*.{}'.format(ext))
-    return l
-
-l = getFile(r'C:\Users\Daniel\Documents\GitHub\StockPortfolio\data', 'csv')
